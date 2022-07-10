800147a4b4a8e0ba9da7c561aff7c4fa4e9afa02
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-07-11 01:19:09 +0400

    updated examples

--- example/nol-menu.py
+++ example/nol-menu.py
@@ -2,44 +2,48 @@ from rtadubai import Nol
 
 Flag = False
 while Flag is False:
-    nol = input('Enter your NOL card number: ').strip().replace(" ", "")
-    if len(nol) == 10:
-        if nol.isdigit():
-            if Nol.isValid(nol) is True:
-                Flag = True
-                break
-    print('Invalid NOL card\n')
+    nol = input("Enter your NOL card number: ")
+    try:
+        card = Nol.Card(nol)
+        Flag = True
+    except ValueError:
+        print("Invalid NOL card\n")
 
 while True:
-    print('\n1. View NOL card Balance\n2. View recent NOL Transaction\n3. View all NOL Transaction\n0. Exit')
+    print("\n1. View NOL card Details\n2. View recent NOL Transaction\n3. View all NOL Transaction\n0. Exit")
 
-    choice = int(input('Enter your choice: '))
+    choice = int(input("Enter your choice: "))
     print()
 
     if choice == 1:
-        print("Card Balance: \t\t", Nol.CardBalance(nol))
+        print("Nol Card: \t\t", card.id)
+        print("Card Balance: \t\t", card.balance)
+        print("Pending Balance: \t", card.pending)
+        print("Expiry Date: \t\t", card.expiry)
+        print()
 
     elif choice == 2:
-        i = Nol.Recent(nol)
-        if i['Error'] is True:
+        transaction = card.recent()
+        if transaction["Error"] is True:
             print("No transactions found")
         else:
-            print('Date:\t\t' + i['Date'])
-            print('Time:\t\t' + i['Time'])
-            print('Type:\t\t' + i['Type'])
-            print('Amount:\t' + i['Amount'])
+            transaction = transaction["Transaction"]
+            print("Date:\t\t" + transaction["Date"])
+            print("Time:\t\t" + transaction["Time"])
+            print("Type:\t\t" + transaction["Type"])
+            print("Amount:\t" + transaction["Amount"] + " AED")
             print()
 
     elif choice == 3:
-        l = Nol.TransactionsRaw(nol)
-        if type(l) is not list:
+        transactions = card.transactions()
+        if transactions["Error"] is True:
             print("No transactions found")
         else:
-            for i in l:
-                print('Date:\t\t' + i['Date'])
-                print('Time:\t\t' + i['Time'])
-                print('Type:\t\t' + i['Type'])
-                print('Amount:\t' + i['Amount'] + ' AED')
+            for transaction in transactions["Transactions"]:
+                print("Date:\t\t" + transaction["Date"])
+                print("Time:\t\t" + transaction["Time"])
+                print("Type:\t\t" + transaction["Type"])
+                print("Amount:\t" + transaction["Amount"] + " AED")
                 print()
     elif choice == 0:
         break
--- example/salik-menu.py
+++ example/salik-menu.py
@@ -4,8 +4,8 @@ plate = input("Enter your vehicle's plate number: ")
 number = input('Enter your mobile number: ')
 
 
-expiry = Salik.Expiry(plate)
-balance = Salik.Balance_Plate(plate, number)
+expiry = Salik.expiry(plate)
+balance = Salik.balance_plate(plate, number)
 
 
 print("Your vehicle's registration expires on:", expiry)
