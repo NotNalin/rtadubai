ff1f5abc586e2bf33cd7f644668bb1afdb10ee18
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-08-28 17:24:03 +0400

    fixed salik balance once again

--- src/rtadubai/Salik.py
+++ src/rtadubai/Salik.py
@@ -3,7 +3,7 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 
-from . import rta_captcha
+import rta_captcha
 
 
 URL = "https://www.rta.ae/wps/portal/rta/ae/home/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zi_QwMTNwNTAx93EPNDAwcQ4MCA8O8gowNXMz1w_Wj9KNASgIMLTycDAx9DIxDnIBKAkO8Ai29PD0MjaEKDHAARwP94NQ8_YLs7DRHR0VFAE1hpMw!/p0/IZ7_KG402B82M83EB0Q64NN5ER3GR6=CZ6_N004G041LGU600AURQQVJR30D7"
@@ -66,7 +66,7 @@ def balance_plate(plate, number, *, AreaCode=1, PlateType=1):
         "MobileNumber": number,
         "language": "en"
     }
-    response = requests.post("https://www.salik.rta.ae/surface/financial/balanceenquiry", data=data).json()
+    response = requests.post("https://www.salik.ae/surface/financial/balanceenquiry", data=data).json()
     if response["Valid"]:
         return response["SalikCredit"]
     else:
