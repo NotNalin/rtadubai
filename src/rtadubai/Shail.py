51a13e09f0f04bc13f34ba737ec52ca78bc0fc59
Nalin <79359909+NotNalin@users.noreply.github.com>
2022-08-28 18:02:33 +0400

    added journey planner(incomplete)

--- src/rtadubai/Shail.py
+++ src/rtadubai/Shail.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 import requests
 from bs4 import BeautifulSoup
 
@@ -39,30 +40,107 @@ class Stop:
         return f"Stop: {self.name}"
 
 
-def departures(stop : Stop):
+def departures(stop: Stop):
     data = {
-        "departureLine" : stop.name,
-        "departureStateless" : stop.id,
-        "departureCoords" : stop.coords,
+        "departureLine": stop.name,
+        "departureStateless": stop.id,
+        "departureCoords": stop.coords,
     }
     response = requests.post(URL + "NJdepartureBoardShailRev=/", data=data)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    d = soup.find(class_ = "jp_departure_result")
+    d = soup.find(class_="jp_departure_result")
     if d is None:
-        if soup.find(class_ = "errorTxt") is not None:
-            raise ValueError(soup.find(class_ = "errorTxt").text)
+        if soup.find(class_="errorTxt") is not None:
+            raise ValueError(soup.find(class_="errorTxt").text)
 
-    d = d.find_all("div", class_ = "jp_departure_item")
+    d = d.find_all("div", class_="jp_departure_item")
     transports = []
     for i in d:
-        raw = i.find_all(class_ = "jp_info")
-        transports.append({
-            "mode" : i.find(class_ = "jp_tmode").text.strip(),
-            "type" : i.find("use").get("xlink:href").split("#")[-1],
-            "direction" : raw[0].b.text.strip(),
-            "scheduled_time" : raw[1].b.text.strip(),
-            "estimated_time" : raw[2].b.text.strip(),
-            "status" : raw[3].b.text.strip()
-        })
+        raw = i.find_all(class_="jp_info")
+        transports.append(
+            {
+                "mode": i.find(class_="jp_tmode").text.strip(),
+                "type": i.find("use").get("xlink:href").split("#")[-1],
+                "direction": raw[0].b.text.strip(),
+                "scheduled_time": raw[1].b.text.strip(),
+                "estimated_time": raw[2].b.text.strip(),
+                "status": raw[3].b.text.strip(),
+            }
+        )
     return transports
+
+
+def journey_planner(
+    fromstop: Stop,
+    tostop: Stop,
+    time=datetime.now(timezone(timedelta(hours=4))),
+    *,
+    depart_or_arrive="D",
+    metro=True,
+    bus=True,
+    tram=True,
+    waterbus=True,
+    avoidchanges=False,
+):
+
+    if depart_or_arrive not in ["D", "A"]:
+        raise ValueError("depart_or_arrive must be either D or A")
+    
+
+    data = {
+        "origin": fromstop.name,
+        "originStateless": fromstop.id,
+        "originCoords": fromstop.coords,
+        "destination": tostop.name,
+        "destinationStateless": tostop.id,
+        "destinationCoords": tostop.coords,
+        "departing": time.strftime("%d/%m/%Y %I:%M %p"),
+        "departorarrive": depart_or_arrive,
+        "jp_plan_dl_depart_hour": time.strftime("%I"),
+        "jp_plan_dl_depart_min": time.strftime("%M"),
+        "jp_plan_dl_depart_merd": time.strftime("%p"),
+        "jp_plan_dl_arrive_hour": time.strftime("%I"),
+        "jp_plan_dl_arrive_min": time.strftime("%M"),
+        "jp_plan_dl_arrive_merd": time.strftime("%p"),
+    }
+    if metro:
+        data["metro"] = "metro"
+    if bus:
+        data["bus"] = "bus"
+    if tram:
+        data["tram"] = "tram"
+    if waterbus:
+        data["waterbus"] = "waterbus"
+    if avoidchanges:
+        data["avoidchanges"] = "avoidchanges"
+
+    response = requests.post(URL + "NJfindTripShailRev=/", data=data)
+    soup = BeautifulSoup(response.text, "html.parser")
+    raw = soup.find(class_="jp_container_results")
+    jps = raw.find_all(class_="jp_container_result")
+    for i in jps:
+        for j in i.find_all("li"):
+            time_ = j.find(class_="jp_row").b.text.strip()
+            stop = j.find(class_="jp_tmode_station").text.strip()
+            try:
+                method = j.find(class_="icon").get("xlink:href").split("#")[-1]
+            except AttributeError:
+                method = None
+            try:
+                mode = j.find(class_="jp_tmode").b.text.strip()
+            except AttributeError:
+                if method == "walk":
+                    mode = "walk"
+                else:
+                    mode = None
+            try:
+                duration = j.find(class_="jp_duration").text.strip()
+            except AttributeError:
+                duration = None
+            print(f"{time_ = }, {stop = }, {method = }, {mode = }, {duration = }")
+        
+        print("\n----------\n")
+
+
+journey_planner(Stop("Rashidiya bus station"), Stop("Al gubhaibha bus station"))
